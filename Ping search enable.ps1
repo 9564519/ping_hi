7ec6cf7a0493227c1e6e83bf99372d7f211fcb96
9564519 <54405203+9564519@users.noreply.github.com>
2024-11-22 02:03:25 +0300

    Add files via upload

---
+++ Ping search enable.ps1
@@ -0,0 +1,17 @@
+# Определяем путь к скрипту
+$scriptDir = Split-Path -Parent $MyInvocation.MyCommand.Path
+
+# Указываем имя подпапки и .exe файла
+$subFolder = "Ping search enable"  # замените на имя вашей папки
+$exeFile = "Ping search enable.exe"  # замените на имя вашего .exe файла
+
+# Формируем полный путь к .exe файлу
+$exePath = Join-Path $scriptDir $subFolder
+$exeFullPath = Join-Path $exePath $exeFile
+
+# Проверяем существует ли файл и запускаем его
+if (Test-Path $exeFullPath) {
+    Start-Process $exeFullPath
+} else {
+    Write-Host "Ошибка: Файл $exeFullPath не найден." -ForegroundColor Red
+}
