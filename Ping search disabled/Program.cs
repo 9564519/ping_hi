80dd8c8bab7ecff63d2a5028cfa0640600374283
9564519 <54405203+9564519@users.noreply.github.com>
2024-11-22 01:56:02 +0300

    Add files via upload

---
+++ Ping search disabled/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace NetworkPingCheck
+{
+    class Program
+    {
+        [DllImport("user32.dll")]
+        public static extern bool SetForegroundWindow(IntPtr hWnd);
+
+        static void Main(string[] args)
+        {
+            Console.Write("Ловим ВЫКЛЮЧЕНИЕ введите IP/ПК: ");
+            string hostName = Console.ReadLine();
+            int timeout = 200; // Время ожидания для каждого пинга (в миллисекундах)
+
+            while (true)
+            {
+                try
+                {
+                    // Получение IP-адреса по сетевому имени
+                    IPHostEntry host = Dns.GetHostEntry(hostName);
+                    string ipAddress = host.AddressList[0].ToString();
+
+                    Ping ping = new Ping();
+                    PingReply reply = ping.Send(ipAddress, timeout);
+
+                    if (reply.Status != IPStatus.Success)
+                    {
+                    // Пинг не удался
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine($"Сетевой узел {hostName} ({ipAddress}) не доступен");
+                    // Выполнение команды "ipconfig /flushdns"
+                    Process ipconfigProcess = new Process();
+                    ipconfigProcess.StartInfo.FileName = "cmd.exe";
+                    ipconfigProcess.StartInfo.Arguments = "/c ipconfig /flushdns";
+                    ipconfigProcess.StartInfo.UseShellExecute = false;
+                    ipconfigProcess.StartInfo.CreateNoWindow = true;
+                    ipconfigProcess.Start();
+                    ipconfigProcess.WaitForExit();
+                        // Мигание окна цветом
+                        for (int i = 0; i < 5; i++)
+                    {
+                        Console.BackgroundColor = Console.BackgroundColor == ConsoleColor.Green ? ConsoleColor.Black : ConsoleColor.Green;
+                        Thread.Sleep(100);
+                    }
+
+                    // Переключение окна в активное состояние
+                    Process currentProcess = Process.GetCurrentProcess();
+                    IntPtr mainWindowHandle = currentProcess.MainWindowHandle;
+                    if (mainWindowHandle != IntPtr.Zero)
+                    {
+                        SetForegroundWindow(mainWindowHandle);
+                    }
+                }
+                else
+                {
+                    // Пинг успешен
+                    Console.BackgroundColor = ConsoleColor.Green;
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine($"Сетевой узел {hostName} ({ipAddress}) доступен");
+
+
+                }
+                }
+                catch (SocketException)
+                {
+                    // Ошибка разрешения сетевого имени
+                    Console.BackgroundColor = ConsoleColor.DarkGray;
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine($"Невозможно разрешить сетевое имя {hostName}");
+                    // Выполнение команды "ipconfig /flushdns"
+                    Process ipconfigProcess = new Process();
+                    ipconfigProcess.StartInfo.FileName = "cmd.exe";
+                    ipconfigProcess.StartInfo.Arguments = "/c ipconfig /flushdns";
+                    ipconfigProcess.StartInfo.UseShellExecute = false;
+                    ipconfigProcess.StartInfo.CreateNoWindow = true;
+                    ipconfigProcess.Start();
+                    ipconfigProcess.WaitForExit();
+                    // Мигание окна цветом
+                    for (int i = 0; i < 5; i++)
+                    {
+                        Console.BackgroundColor = Console.BackgroundColor == ConsoleColor.Green ? ConsoleColor.Black : ConsoleColor.Green;
+                        Thread.Sleep(100);
+                    }
+
+                    // Переключение окна в активное состояние
+                    Process currentProcess = Process.GetCurrentProcess();
+                    IntPtr mainWindowHandle = currentProcess.MainWindowHandle;
+                    if (mainWindowHandle != IntPtr.Zero)
+                    {
+                        SetForegroundWindow(mainWindowHandle);
+                    }
+
+                }
+                Console.ResetColor();
+                Thread.Sleep(400); // Задержка перед следующей проверкой
+            }
+        }
+    }
+}
